e5a4eff2bf2e750ea8b1c59352fee07d9baf31e5
vladimir.borodin <vborodin@yandex.ru>
2022-09-23 16:20:02 +0700

    Add project files.

---
+++ HelloWorld/Controllers/HelloController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    [ApiController]
+    [Route("/hello")]
+    public class HelloController
+    {
+        [HttpGet]
+        public string GetHello()
+        {
+            return "Hello, World!";
+        }
+    }
+}
