f75fd8eb71c87f8cf425e820eadb8e21bffdff7a
vladimir.borodin <vborodin@yandex.ru>
2022-09-25 18:54:12 +0700

    add secured endpoint

--- HelloWorld/Controllers/HelloController.cs
+++ HelloWorld/Controllers/HelloController.cs
@@ -1,13 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HelloWorld.Controllers
 {
     [ApiController]
     [Route("/hello")]
-    public class HelloController
+    public class HelloController: ControllerBase
     {
         [HttpGet]
         public string GetHello()
+        {
+            return "Hello, World!";
+        }
+
+        [HttpGet]
+        [Route("secured")]
+        [Authorize]
+        public string GetHelloSecured()
         {
             return "Hello, World!";
         }
--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options => builder.Configuration.Bind("JwtSettings", options));
 
 var app = builder.Build();
 
