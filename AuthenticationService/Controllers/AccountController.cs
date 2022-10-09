b11d0ae2b20d43da512689a2442d6b560df635e3
vladimir.borodin <vborodin@yandex.ru>
2022-10-09 16:22:27 +0700

    LoginController is renamed to AccountController

--- AuthenticationService/Controllers/AccountController.cs
+++ AuthenticationService/Controllers/AccountController.cs
@@ -9,18 +9,18 @@ namespace AuthenticationService.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class LoginController : ControllerBase
+public class AccountController : ControllerBase
 {
     private readonly IUserService userService;
     private readonly ITokenGenerator<UserModel> tokenGenerator;
 
-    public LoginController(IUserService userService, ITokenGenerator<UserModel> tokenGenerator)
+    public AccountController(IUserService userService, ITokenGenerator<UserModel> tokenGenerator)
     {
         this.userService = userService;
         this.tokenGenerator = tokenGenerator;
     }
 
-    [HttpPost]
+    [HttpPost("Login")]
     [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(string))]
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     public IActionResult Login([FromBody] UserLogin userLogin, [FromQuery] string audience, [FromQuery] int expirationPeriodMinutes = 15)
