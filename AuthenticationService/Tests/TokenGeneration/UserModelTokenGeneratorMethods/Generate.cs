c0e4e68b49e60fffadc275bb9c89ac3265b36678
Vladimir Borodin <vborodin@yandex.ru>
2022-10-18 17:05:31 +0700

    UserModelTokenGenerator tests updated

---
+++ AuthenticationService/Tests/TokenGeneration/UserModelTokenGeneratorMethods/Generate.cs
@@ -0,0 +1,77 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+using Microsoft.IdentityModel.Tokens;
+
+using NUnit.Framework;
+
+namespace AuthenticationService.Tests.TokenGeneration.UserModelTokenGeneratorMethods;
+
+public class Generate: UserModelTokenGeneratorTest
+{
+    [Test]
+    public void GeneratesValidHeader()
+    {
+        var token = Decode(
+            this.generator.Generate(this.data, "1", 1));
+        
+        Assert.AreEqual("HS256", token.Header.Alg);
+        Assert.AreEqual("JWT", token.Header.Typ);
+    }
+
+    [Test]
+    public void GeneratesValidPayload()
+    {
+        var expirationPeriodMinutes = 1;
+        var audience = "1,2";
+
+        var token = Decode(
+            this.generator.Generate(this.data, "1,2", expirationPeriodMinutes));
+
+        var nameIdentifier = token.Payload[ClaimTypes.NameIdentifier] as string;
+        var emailAddress = token.Payload[ClaimTypes.Email] as string;
+        var givenName = token.Payload[ClaimTypes.GivenName] as string;
+        var surname = token.Payload[ClaimTypes.Surname] as string;
+        var role = token.Payload[ClaimTypes.Role] as string;
+        Assert.AreEqual(audience, token.Payload.Aud.Single());
+        Assert.AreEqual(this.issuer, token.Payload.Iss);
+        Assert.Less(DateTime.UtcNow, token.Payload.ValidTo);
+        Assert.Greater(DateTime.UtcNow.AddMinutes(expirationPeriodMinutes), token.Payload.ValidTo);
+        Assert.AreEqual(this.data.Username, nameIdentifier);
+        Assert.AreEqual(this.data.Email, emailAddress);
+        Assert.AreEqual(this.data.GivenName, givenName);
+        Assert.AreEqual(this.data.Surname, surname);
+        Assert.AreEqual(this.data.Role, role);
+    }
+
+    [Test]
+    public void GeneratesValidSignature()
+    {
+        var token = this.generator.Generate(this.data, "1", 1);
+
+        var handler = new JwtSecurityTokenHandler();
+        var validationParameters = new TokenValidationParameters()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = issuer,
+            ValidAudience = "1",
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(this.securityKey))
+        };
+        ClaimsPrincipal? claimsPrincipal = null;
+        Assert.DoesNotThrow(() =>
+        {
+            claimsPrincipal = handler.ValidateToken(token, validationParameters, out _);
+        });
+        Assert.NotNull(claimsPrincipal);
+    }
+
+    private JwtSecurityToken Decode(string token)
+    {
+        var handler = new JwtSecurityTokenHandler();
+        return handler.ReadJwtToken(token);
+    }
+}
---
+++ AuthenticationService/Tests/TokenGeneration/UserModelTokenGeneratorMethods/UserModelTokenGeneratorTest.cs
@@ -0,0 +1,32 @@
+﻿using AuthenticationService.Services.Model;
+using AuthenticationService.Services.TokenGenerator;
+
+using NUnit.Framework;
+
+namespace AuthenticationService.Tests.TokenGeneration.UserModelTokenGeneratorMethods;
+
+public abstract class UserModelTokenGeneratorTest
+{
+    protected readonly string securityKey = "1111111111111111";
+    protected readonly string issuer = "issuer";
+    protected ITokenGenerator<UserModel> generator = null!;
+    protected UserModel data = null!;
+
+    [SetUp]
+    public void Setup()
+    {
+        this.generator = new UserModelTokenGenerator(this.securityKey, this.issuer);
+        this.data = CreateTestData();
+    }
+
+    private UserModel CreateTestData()
+    {
+        return new UserModel(
+            Email: "TestEmail",
+            GivenName: "TestGivenName",
+            Role: "TestRole",
+            Surname: "TestSurname",
+            Username: "TestUsername"
+        );
+    }
+}
