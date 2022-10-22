f7a03c9af6915a36dca984ab2348d9b21ba55a94
Vladimir Borodin <vborodin@yandex.ru>
2022-10-22 11:19:58 +0700

    RoleFilter

---
+++ AuthenticationService/Repository/Filter/RoleFilter.cs
@@ -0,0 +1,18 @@
+﻿using AuthenticationService.Repository.Entities;
+
+namespace AuthenticationService.Repository.Filter;
+
+public class RoleFilter : IFilter<RoleEntity>
+{
+    private readonly string role;
+
+    public RoleFilter(string role)
+    {
+        this.role = role;
+    }
+
+    public IQueryable<RoleEntity> Apply(IQueryable<RoleEntity> source)
+    {
+        return source.Where(x => x.Role == this.role);
+    }
+}
