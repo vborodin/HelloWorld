a75472a26a3deb1f5598fa3ace2687f7006a2c25
Vladimir Borodin <vborodin@yandex.ru>
2022-10-23 17:36:47 +0700

    RoleController

---
+++ AuthenticationService/Controllers/Dtos/RoleDto.cs
@@ -0,0 +1,3 @@
+﻿namespace AuthenticationService.Controllers.Dtos;
+
+public record RoleDto(string Role);
---
+++ AuthenticationService/Tests/Controllers/RoleControllerMethods/CreateRoleAsync.cs
@@ -0,0 +1,37 @@
+﻿using AuthenticationService.Controllers.Dtos;
+using AuthenticationService.Services.Exceptions;
+
+using Microsoft.AspNetCore.Mvc;
+
+using Moq;
+
+using NUnit.Framework;
+
+namespace AuthenticationService.Tests.Controllers.RoleControllerMethods;
+
+public class CreateRoleAsync : RoleControllerTest
+{
+    [Test]
+    public async Task CreatesRole()
+    {
+        var result = await this.roleController.CreateRoleAsync(
+            roleDto: new RoleDto("NewRole"));
+
+        Assert.True(result is OkResult);
+        this.roleServiceMock.Verify(m => m.CreateRoleAsync("NewRole"), Times.Once);
+    }
+
+    [Test]
+    public async Task RequiresRoleToBeUnique()
+    {
+        this.roleServiceMock
+            .Setup(m => m.CreateRoleAsync(It.IsAny<string>()))
+            .Throws<RoleExistenceException>();
+
+        var result = await this.roleController.CreateRoleAsync(
+            roleDto: new RoleDto("ExistingRole"));
+
+        Assert.True(result is BadRequestObjectResult);
+        this.roleServiceMock.Verify(m => m.CreateRoleAsync("ExistingRole"), Times.Once);
+    }
+}
